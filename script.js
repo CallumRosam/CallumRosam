4ae11a715523ee5c0e35d278defe9c5c9303453e
CallumRosam <104946983+CallumRosam@users.noreply.github.com>
2022-05-24 18:52:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -68,3 +68,24 @@ document.getElementById('noun2').addEventListener('click', (event) => {
   event.target.innerText = randomMember(getWords('WORD'));
 
 });
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+
+document.getElementById('post').addEventListener('click', (event) => {
+  let element_unordered = document.getElementById('unordered');
+  let new_ul = document.createElement('ul');
+  new_ul.innerText = getNumberOrString(document.getElementById('comment-box').value);
+
+  element_unordered.appendChild(new_ul);
+
+});
